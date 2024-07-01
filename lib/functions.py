97fcc2ac7668b5ea60b1ab5366e954c6c230ec98
Tumaini <tumainikebaya.234@gmail.com>
2024-07-01 22:00:43 +0300

    Adds functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+greet_programmer()    
+
+def greet(name = "Gudio"):
+    print(f"Hello, {name}!")
+    
+greet("Gudio")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+greet_with_default()
+greet_with_default("Gudio")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+print(add(2, 3))
 
 def halve(number):
-    pass
+    return number / 2
+
+print(halve(4))
